c9ca246961f60e95aafe6ede2eff53fdac42ab70
eason <oncwnuIh8Q1tswHbmxrWCoV79Yik@git.weixin.qq.com>
2019-11-30 22:03:02 +0800

    check

---
+++ travel/src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <div>
+    home
+    <router-link to="/list">list
+    </router-link>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+</style>
